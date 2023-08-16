92b396d387dace5e0cb55222f92d6da46358e6ce
seohwa0325 <seohwa0325@gmail.com>
2023-08-16 11:39:33 +0900

    uploadPage

---
+++ src/app/products/upload/page.tsx
@@ -0,0 +1,13 @@
+import Input from 'postcss/lib/input'
+import React, { useState } from 'react'
+
+const ProductUploadPage = () => {
+    // const [isLoading , setIsLoading] = useState
+  return (
+    <div>
+ 
+    </div>
+    )
+}
+
+export default ProductUploadPage
