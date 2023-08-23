7b582a44255327235637e50d846a71852d74764b
seohwa0325 <seohwa0325@gmail.com>
2023-08-23 16:58:44 +0900

    upload_page

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -2,18 +2,83 @@
 import React from 'react'
 import { useState } from 'react';
 import Input from '../../components/Input'
+import { FieldValues, useForm } from 'react-hook-form';
 
 const ProductUploadPage = () => {
-    const [isLoading , setIsLoading] = useState("")
+  const [isLoading, setIsLoading] = useState("")
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: {
+      errors,
+    },
+    reset
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: '',
+      description: '',
+      category: '',
+      latitude: 33.5563,
+      longitude: 126.79581,
+      imagSrc: '',
+      price: 1
+    }
+  })
   return (
     <div>
-      <Input 
-      id ="title"
-      label ="Title"
-      disalbed ={isLoading} >fdfd</Input>
-    
+      <Input
+        id="title"
+        label="Title"
+        disalbed={isLoading}
+        register={register}
+        errors={errors}
+        required >
+      </Input>
+      <hr />
+      <Input
+        id="description"
+        label="Description"
+        disalbed={isLoading}
+        register={register}
+        errors={errors}
+        required >
+      </Input>
+      <hr />
+      <Input
+        id="price"
+        label="Price"
+        disalbed={isLoading}
+        register={register}
+        errors={errors}
+        required >
+      </Input>
+      <hr />
+      <Input
+        id="title"
+        label="Title"
+        disalbed={isLoading}
+        register={register}
+        errors={errors}
+        required >
+      </Input>
+      <hr />
+      <div className='
+      grid
+      grid-cols-1
+      md:grid-cols-2
+      gap-3
+      max-h-[50wh]
+      overflow-y-auto'>
+        {/* catagory */}
+      </div>
+      <hr/>
+
+
     </div>
-    )
+  )
 }
 
 export default ProductUploadPage
