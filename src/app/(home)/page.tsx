2b182fabc4c3dcdd5bafdbc736f3177fef2f2500
seohwa0325 <seohwa0325@gmail.com>
2023-09-21 10:53:33 +0900

    remarket revise

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+// import getProducts, { ProductParams } from '../actions/getProducts'
 
-export default function Home() {
-  
+interface HomeProps {
+  searchParmas: ProductParams
+}
+export default async function Home({searchParmas} : HomeProps) {
+    // const products = await getProducts(searchParmas)
+
   return (
     <main>
          <p> 누구나 볼 수 있는 페이지 입니다.</p>
