3d6b8aa6b2591b67d0f808f92ae483086a9b13bc
seohwa0325 <seohwa0325@gmail.com>
2023-09-22 16:39:58 +0900

    nextauth

--- src/app/components/NavItem.tsx
+++ src/app/components/NavItem.tsx
@@ -1,14 +1,20 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 const NavItem = ({mobile}: {mobile?: boolean}) => {
+  const {data : session, status} =useSession();
+  console.log({session}, status)
   return (
     <ul className='text-md justify-center flex gap-4 w-full items-center ${mobile && "full-col h-full"}'>
         <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>Admin</Link></li>
         <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/user'>User</Link></li>
-        <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signout</button></li>
-        <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signin</button></li>
-
+       {session?.user
+       ?
+        <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={() => signOut()}>Signout</button></li>
+        :
+        <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={() => signIn()}>Signin</button></li>
+       }
     </ul>
   )
 }
