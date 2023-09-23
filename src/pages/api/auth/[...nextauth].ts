569207aa91b2667f6ef7bb428edc32e51074fc7d
seohwa0325 <seohwa0325@gmail.com>
2023-09-23 13:29:39 +0900

    인증

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -60,6 +60,10 @@ export const authOptions :NextAuthOptions = {
     ],session :{
         strategy : 'jwt'
     },
+     jwt: {
+        secret: 'secret',
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
     callbacks: {
 
         async jwt({ token, user }) {
